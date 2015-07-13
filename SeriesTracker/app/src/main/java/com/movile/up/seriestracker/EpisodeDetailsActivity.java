b4872d9bf400297196e9b5a2654109a3d18b84cf
João Vitor Araki Gonçalves <joao_vitor_araki_goncalves@hotmail.com>
2015-07-13 13:01:43 -0300

    project initial commit

---
+++ SeriesTracker/app/src/main/java/com/movile/up/seriestracker/EpisodeDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.movile.up.seriestracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class EpisodeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.episode_details_activity);
+    }
+
+}
